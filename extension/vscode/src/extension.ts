56837d2402f154537ba1a57a0a4f47188318ecc1
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-01-18 13:24:52 -0500

    npm install; new typescript

--- extension/vscode/src/extension.ts
+++ extension/vscode/src/extension.ts
@@ -36,7 +36,6 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
-
 }
 
 // this method is called when your extension is deactivated
