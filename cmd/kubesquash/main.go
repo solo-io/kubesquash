e37302dc04d2d157cfaf285f21c05fbb780e4570
Bartosz Majsak <bartosz.majsak@gmail.com>
2019-01-23 16:56:10 +0100

    chore(typos): fixes typos in help msgs

--- cmd/kubesquash/main.go
+++ cmd/kubesquash/main.go
@@ -8,10 +8,12 @@ import (
 	"github.com/solo-io/kubesquash/pkg/cmd"
 )
 
-const descriptionUsage = `Normally squash lite requires no arguments. just run it!
-it works by creating additional privileged debug pod and then attaching to it. 
-Kubernetes with CRI is needed. Due to a technical limitation, kubesquash doesn't support 
-scratch images at the moment (squash lite relys on the 'ls' command present in the image). 
+const descriptionUsage = `Normally KubeSquash requires no arguments. Just run it!
+It works by creating additional privileged debug pod and then attaching to it.
+
+IMPORTANT:
+  * Kubernetes with CRI is needed - see https://kubernetes.io/docs/setup/cri/.
+  * Due to a technical limitation, KubeSquash doesn't support scratch images at the moment (KubeSquash relies on the 'ls' command present in the image).
 `
 
 func main() {
@@ -22,10 +24,10 @@ func main() {
 		flag.PrintDefaults()
 	}
 
-	flag.BoolVar(&cfg.NoClean, "no-clean", false, "don't clean temporar pod when existing")
+	flag.BoolVar(&cfg.NoClean, "no-clean", false, "don't clean temporary pod when existing")
 	flag.BoolVar(&cfg.ChooseDebugger, "no-guess-debugger", false, "don't auto detect debugger to use")
 	flag.BoolVar(&cfg.ChoosePod, "no-guess-pod", false, "don't auto detect pod to use")
-	flag.BoolVar(&cfg.NoDetectSkaffold, "no-detect-pod", false, "don't auto settigns based on skaffold configuration present in current folder")
+	flag.BoolVar(&cfg.NoDetectSkaffold, "no-detect-pod", false, "don't auto settings based on skaffold configuration present in current folder")
 	flag.BoolVar(&cfg.DebugServer, "debug-server", false, "start a debug server instead of an interactive session")
 	flag.IntVar(&cfg.TimeoutSeconds, "timeout", 300, "timeout in seconds to wait for debug pod to be ready")
 	flag.StringVar(&cfg.DebugContainerVersion, "container-version", cmd.ImageVersion, "debug container version to use")
